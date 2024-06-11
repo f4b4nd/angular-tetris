63a954842ca49507b8ba5accdb28bf5077afff7c
fabien <fabien.andrian@gmail.Com>
2024-06-11 19:33:51 +0200

    switch to scss

--- angular-tetris/src/app/components/buttons/power-switch-button.component.ts
+++ angular-tetris/src/app/components/buttons/power-switch-button.component.ts
@@ -2,54 +2,58 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'power-switch-button',
-    styles: [
+    styles: 
         `
+        $grey: #ccc;
+        $blue: #2196F3;
+        $red: orange;
+
         .switch {
             position: relative;
             display: inline-block;
             width: 60px;
             height: 34px;
           }
-        `,
-        `
+        
         .switch input {
             opacity: 0;
             width: 0;
             height: 0;
           }
-        `,
-        `
+     
+        
         .slider {
+
             position: absolute;
             cursor: pointer;
             top: 0;
             left: 0;
             right: 0;
             bottom: 0;
-            background-color: #ccc;
+            background-color: green;
             -webkit-transition: .4s;
             transition: .4s;
-        }`,
-        `
-        .slider:before {
-            position: absolute;
-            content: "";
-            height: 26px;
-            width: 26px;
-            left: 4px;
-            bottom: 4px;
-            background-color: red;
-            -webkit-transition: .4s;
-            transition: .4s;
-          }
-        `,
-        `
+
+            &:before {
+                position: absolute;
+                content: "";
+                height: 26px;
+                width: 26px;
+                left: 4px;
+                bottom: 4px;
+                background-color: $red;
+                -webkit-transition: .4s;
+                transition: .4s;
+            }
+        }
+        
+        
         input:checked + .slider {
-            background-color: #ccc;
+            background-color: $grey;
         }
           
         input:focus + .slider {
-            box-shadow: 0 0 1px #2196F3;
+            box-shadow: 0 0 1px $grey;
         }
 
         input:checked + .slider:before {
@@ -59,15 +63,15 @@ import { Component } from '@angular/core'
         }
 
         .slider.round {
+
             border-radius: 34px;
+
+            &::before {
+                border-radius: 50%;
+            }
         }
 
-        .slider.round:before {
-            border-radius: 50%;
-        }
-        `
-
-    ],
+    `,
     template: `
         <label class="switch">
             <input 
